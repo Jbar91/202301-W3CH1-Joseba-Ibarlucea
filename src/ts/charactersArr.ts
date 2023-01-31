06f706bc2f62ad6bd44a8435acffa56cc9665102
Joseba Ibarlucea <Jibarlucea91@gmail.com>
2023-01-31 01:07:06 +0100

    Added ts folder and added squire module

---
+++ src/ts/charactersArr.ts
@@ -0,0 +1,60 @@
+import { Brawler } from './brawler.js';
+import { King } from './king.js';
+import { RightHand } from './rightHand.js';
+import { Squire } from './squire.js';
+
+const joffrey = new King({
+  series: 'Juego de Tronos',
+  name: 'Joffrey Baratheon',
+  family: 'Baratheon',
+  age: 19,
+  isAlive: true,
+  reign: 1,
+});
+
+const jaime = new Brawler({
+  series: 'Juego de Tronos',
+  name: 'Jamie Lannister',
+  family: 'Lannister',
+  age: 34,
+  isAlive: true,
+  weapon: 'Sword',
+});
+
+const daenerys = new Brawler({
+  series: 'Juego de Tronos',
+  name: 'Daenerys Targaryen',
+  family: 'Targaryen',
+  age: 22,
+  isAlive: true,
+  weapon: 'Dragon',
+});
+
+const tyrion = new RightHand({
+  series: 'Juego de Tronos',
+  name: 'Tyrion Lannister',
+  family: 'Lannister',
+  age: 27,
+  isAlive: true,
+  assess: 'Daenerys Targaryen',
+});
+
+const bronn = new Squire({
+  series: 'Juego de Tronos',
+  name: 'Bronn',
+  family: 'Bronn',
+  age: 35,
+  isAlive: true,
+  knight: 'Jamie Lannister',
+});
+
+export const charactersArr = [joffrey, jaime, daenerys, tyrion, bronn];
+
+console.log(
+  charactersArr,
+  joffrey.communicate(),
+  joffrey.state(),
+  joffrey.perish(),
+  joffrey.state(),
+  jaime.skill()
+);
