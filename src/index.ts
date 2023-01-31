221a03cc4e1fbb01b3d48837de2b813d42361e69
Joseba Ibarlucea <Jibarlucea91@gmail.com>
2023-01-31 21:32:41 +0100

    Add first renders on index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-import { charactersArr } from './ts/charactersArr';
+import { CardContainer } from './components/card-container/card-container';
+import { CharactersList } from './components/characters-list/characters-list';
+import { charactersArr } from './mocks/charactersData';
 
 console.log(charactersArr);
+
+const cardContainer = new CardContainer('#app');
+const characterList = new CharactersList('.characters-list');
+
+cardContainer.render('afterbegin');
+characterList.render('afterbegin');
